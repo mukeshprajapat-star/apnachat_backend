ebc3aa22ff346968a1817c4250b91f32acbf50ec
mukeshprajapat-star <mukeshprajapat7529@gmail.com>
2024-06-25 10:44:28 +0530

    done

--- app.js
+++ app.js
@@ -68,10 +68,10 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/chat", chatRoute);
 app.use("/api/v1/admin", adminRoute);
 
-app.use(express.static(path.join(__dirname,"../client/dist")));
+app.use(express.static(path.join(__dirname,"../../client/dist")));
 
 app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"../client/dist/index.html"));
+    res.sendFile(path.resolve(__dirname,"../../client/dist/index.html"));
 });
 
 app.get("/", (req, res) => {
